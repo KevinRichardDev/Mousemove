d598ed7eb909f44349a53273e15f396b182f1ce7
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-20 07:43:56 +0100

    Partie JS terminée

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
-
-// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
-
-// S'assurer que les liens sont clickables
-
-// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
-
-// const circle1 = document.querySelector(".circle1");
-// const circle2 = document.querySelector(".circle2");
-
-// window.addEventListener("mousemove", (e) => {
-//   circle1.style.left = e.pageX + "px";
-//   circle1.style.top = e.pageY + "px";
-// })
-
 const cursor = document.getElementById("cursor");
 const mouse1 = document.getElementById("mouse1");
 const mouse2 = document.getElementById("mouse2");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.pageX + "px";
+  cursor.style.top = e.pageY + "px";
+
+  mouse1.style.left = e.pageX + "px";
+  mouse1.style.top = e.pageY + "px";
+
+  mouse2.style.left = e.pageX + "px";
+  mouse2.style.top = e.pageY + "px";
+})
+
+// S'assurer que les liens sont clickables

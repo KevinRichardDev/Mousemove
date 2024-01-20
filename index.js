5f338c7a72e54a77ee2703543cd4c99b83189599
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-20 07:50:45 +0100

    Opitmisation du JS et fin du projet

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const cursor = document.getElementById("cursor");
-const mouse1 = document.getElementById("mouse1");
-const mouse2 = document.getElementById("mouse2");
+const mouses = document.querySelectorAll('.mouse');
 
 window.addEventListener("mousemove", (e) => {
-  cursor.style.left = e.pageX + "px";
-  cursor.style.top = e.pageY + "px";
-
-  mouse1.style.left = e.pageX + "px";
-  mouse1.style.top = e.pageY + "px";
-
-  mouse2.style.left = e.pageX + "px";
-  mouse2.style.top = e.pageY + "px";
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  })
 })
+
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.pageX + "px";
+//   cursor.style.top = e.pageY + "px";
+
+//   mouse1.style.left = e.pageX + "px";
+//   mouse1.style.top = e.pageY + "px";
+
+//   mouse2.style.left = e.pageX + "px";
+//   mouse2.style.top = e.pageY + "px";
+// })
 
 // S'assurer que les liens sont clickables

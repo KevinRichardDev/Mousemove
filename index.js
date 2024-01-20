728c5587209cbab67a85fe1292a18e3128e5bcb4
KevinRichardDev <kevin.richard0@orange.fr>
2024-01-20 07:31:22 +0100

    Partie HTML & CSS

--- index.js
+++ index.js
@@ -5,3 +5,15 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+// const circle1 = document.querySelector(".circle1");
+// const circle2 = document.querySelector(".circle2");
+
+// window.addEventListener("mousemove", (e) => {
+//   circle1.style.left = e.pageX + "px";
+//   circle1.style.top = e.pageY + "px";
+// })
+
+const cursor = document.getElementById("cursor");
+const mouse1 = document.getElementById("mouse1");
+const mouse2 = document.getElementById("mouse2");
